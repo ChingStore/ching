2cc542b8655fb143a2f078925e50210861e582f6
Roman Pavlovskyi <roman@pumpup.co>
2018-10-26 21:24:29 +0200

    :star: Add web3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import web3 from 'web3'
+
 class App extends Component {
+
+  componentWillMount() {
+    console.log(web3);
+  }
+
   render() {
     return (
       <div className="App">
