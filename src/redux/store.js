bfe496682a8e974c3e07e1e21c5526216291bee8
pycckuu <is.markelov@gmail.com>
2018-12-19 09:30:40 +0100

    redux-devtool as a package

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,17 +4,11 @@ import ReduxLogger from 'redux-logger';
 import rootReducer from './reducers';
 import localStorageUtil from '../utils/localStorage';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const persistedState = localStorageUtil.loadState();
 
-const store = createStore(
-  rootReducer,
-  persistedState,
-  compose(
-    applyMiddleware(thunk, ReduxLogger),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, persistedState, composeWithDevTools(applyMiddleware(thunk, ReduxLogger)));
 
 store.subscribe(() => {
   localStorageUtil.saveState(store.getState());
