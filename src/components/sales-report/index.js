fe7e7b45bf08cf8c121e050c640fd544a47925e3
Aaron Anderson <aaronlynanderson@gmail.com>
2018-12-20 15:31:40 -0600

    Update src/components/sales-report/index.js

--- src/components/sales-report/index.js
+++ src/components/sales-report/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SalesTable from './SalesTable';
+import SalesTable from './salesTable';
 import { connect } from 'react-redux';
 import selectors from '../../redux/selectors';
 
