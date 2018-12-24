1e59a628d40e6ffe0ca7f74e7774ab908442e00d
Aaron Anderson <aaronlynanderson@gmail.com>
2018-12-23 22:54:39 -0600

    :construction: [login-view] attempt to pass authentication status through store to header

--- src/redux/reducers/authentication.js
+++ src/redux/reducers/authentication.js
@@ -1,22 +1,22 @@
 import ACTIONS from "../actionTypes";
 
-const initialState = {};
+const initialState = { authenticated: "false" };
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case ACTIONS.SIGN_IN: {
       const { ...authenticationFields } = action.payload;
       const newState = Object.assign({}, state);
-      newState.authenticated = true;
+      newState.authenticated = "true";
       return Object.assign({}, newState);
     }
     case ACTIONS.SIGN_UP: {
       const { ...authenticationFields } = action.payload;
       const newState = Object.assign({}, state);
-      newState.authenticated = true;
+      newState.authenticated = "true";
       return Object.assign({}, newState);
     }
     default:
-      return (state.authenticated = false);
+      return initialState;
   }
 }
