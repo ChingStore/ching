7a6e807b2426182f269ade6f81dba19df891ef2c
Aaron Anderson <aaronlynanderson@gmail.com>
2018-12-26 22:13:50 -0600

    :bug: [login-view] fix error: 'authenticationFields' is assigned a value but never used

--- src/redux/reducers/authentication.js
+++ src/redux/reducers/authentication.js
@@ -1,25 +1,17 @@
-import ACTIONS from "../actionTypes";
+import ACTIONS from '../actionTypes'
 
-const initialState = { authenticated: "false" };
+const initialState = { authenticated: 'false' }
 
 export default function(state = initialState, action) {
-  console.log("AUTHENTICATION.JS!");
+  console.log('AUTHENTICATION.JS!')
   switch (action.type) {
-    case ACTIONS.SIGN_IN: {
-      console.log("SIGN_IN");
-      const { ...authenticationFields } = action.payload;
-      const newState = Object.assign({}, state);
-      newState.authenticated = "true";
-      return Object.assign({}, newState);
-    }
-    case ACTIONS.SIGN_UP: {
-      console.log("SIGN_UP");
-      const { ...authenticationFields } = action.payload;
-      const newState = Object.assign({}, state);
-      newState.authenticated = "true";
-      return Object.assign({}, newState);
-    }
+    case ACTIONS.SIGN_IN:
+      return console.log('SIGN_IN')
+
+    case ACTIONS.SIGN_UP:
+      return console.log('SIGN_UP')
+
     default:
-      return initialState;
+      return initialState
   }
 }
