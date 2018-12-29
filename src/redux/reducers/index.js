71f78da023a44627c67e3505822034cc00e7cd73
pycckuu <is.markelov@gmail.com>
2018-12-30 02:21:43 +0400

    :tada:

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import * as Redux from 'redux'
 import * as ReduxFirestore from 'redux-firestore'
+import * as ReactReduxFirebase from 'react-redux-firebase'
 
 export default Redux.combineReducers({
   firestore: ReduxFirestore.firestoreReducer,
+  firebase: ReactReduxFirebase.firebaseReducer,
 })
