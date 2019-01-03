a45078faf8b854706896c4aeaca16dafb91edad8
Aaron Anderson <aaronlynanderson@gmail.com>
2019-01-03 08:50:09 -0600

    :heavy_plus_sign: [login-view] import PropTypes

--- src/components/sales-report/sales-table.js
+++ src/components/sales-report/sales-table.js
@@ -7,6 +7,7 @@ import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
+import PropTypes from 'prop-types'
 
 import { Component } from 'react'
 
