b1059163a0437579745a294d96493fd30c445e11
pycckuu <is.markelov@gmail.com>
2019-01-10 02:52:50 +0100

    :star: async/await

--- src/redux/actions/item.js
+++ src/redux/actions/item.js
@@ -1,39 +1,41 @@
 import ACTIONS from '../actionTypes'
 
 const add = ({ name, picture, soldCount, quantity, price }) => {
-  return (dispatch, getState, { getFirestore }) => {
+  return async (dispatch, getState, { getFirestore, getFirebase }) => {
     const firestore = getFirestore()
-    firestore
-      .collection('items')
-      .add({
+    const firebase = getFirebase()
+    const state = getState()
+    try {
+      await firestore.collection('items').add({
         name,
         picture,
         soldCount,
         quantity,
         price,
+        userId: state.firebase.auth.uid,
         createdAt: new Date(),
       })
-      .then(() => {
-        dispatch({
-          type: ACTIONS.ADD_ITEM_SUCCESS,
-        })
+      dispatch({
+        type: ACTIONS.ADD_ITEM_SUCCESS,
       })
-      .catch(err => {
-        dispatch({
-          type: ACTIONS.ERROR,
-          err,
-        })
+    } catch (err) {
+      dispatch({
+        type: ACTIONS.ERROR,
+        err,
       })
+    }
   }
 }
 
-const sell = (id, quantity) => {
-  return dispatch => {
-    dispatch({ type: ACTIONS.SELL_ITEM, payload: { id, quantity } })
+const testFunction = (id, quantity) => {
+  return async (dispatch, getState, { getFirestore, getFirebase }) => {
+    const items = await getFirestore().collection('items')
+    const test_item = items.where('name', '==', 'test').orderBy('createdAt')
+    console.log(test_item)
   }
 }
 
 export default {
   add,
-  sell,
+  testFunction,
 }
