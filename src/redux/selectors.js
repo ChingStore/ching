ade7d22a788a9c4b1caa2f0ed60661a350d384fd
pycckuu <is.markelov@gmail.com>
2019-01-10 02:53:12 +0100

    stores and users selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,11 @@
 const getItemsState = store => store.firestore.data.items
 const getAuthState = store => store.firebase.auth
+const getStores = store => store.firebase.data.stores
+const getStoresUsers = store => store.firebase.data.storesUsers
 
 export default {
   getItemsState,
   getAuthState,
+  getStores,
+  getStoresUsers,
 }
