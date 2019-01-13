f74f09deb2758a0b27f94bd4ae7d45eb09072f2e
pycckuu <is.markelov@gmail.com>
2019-01-13 13:22:31 +0100

    :memo: adding some note on the required request url

--- firebase-functions/index.js
+++ firebase-functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+
 admin.initializeApp(functions.config().firebase)
 
 const reportTransaction = (orderId, txHash) => {
@@ -13,7 +14,9 @@ const reportTransaction = (orderId, txHash) => {
 }
 
 exports.transactionBuffer = functions.https.onRequest((request, response) => {
-  console.log('The query:')
-  console.log(request.query)
+  // NOTE: Request should be in the followinig form:
+  // https://us-central1-daipos.cloudfunctions.net/transactionBuffer?orderid=prJgCFcSRIJsOMFuM6Bo&txHash=0x2946052ac5fb89124ef233645cae8cdd4e150dcd86d94508a2f0e5487fe63cc4
+
+  console.log('The query:', request.query)
   reportTransaction(request.query.orderId, request.query.txHash)
 })
