8cc11b9d19dbaea876c25daf45563dd70a966673
pycckuu <is.markelov@gmail.com>
2019-01-13 23:23:24 +0100

    :star: views

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,7 +2,7 @@ const getItemsState = store => store.firestore.data.items
 const getAuthState = store => store.firebase.auth
 const getStores = store => store.firestore.data.stores
 const getStoresUsers = store => store.firestore.data.storesUsers
-const getOrders = store => store.firestore.data.orders
+const getOrders = store => store.firestore.ordered.orders
 
 export default {
   getItemsState,
