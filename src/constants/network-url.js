02ec545b7625220bde2fd6d4bc4eb6295fb6f3d7
pycckuu <is.markelov@gmail.com>
2019-01-24 22:24:51 +0100

    :truck: moved newtwork uri in config file

---
+++ src/constants/network-url.js
@@ -0,0 +1,9 @@
+const NETWORK_URL = {
+  MAINNET: 'https://mainnet.infura.io/v3/3059c072371d4397b84e9577f896d91c',
+  KOVAN: 'https://kovan.infura.io/v3/3059c072371d4397b84e9577f896d91c',
+  XDAI: 'https://poa.infura.io',
+}
+
+export default {
+  NETWORK_URL,
+}
