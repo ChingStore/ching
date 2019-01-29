a7df5e7623c564cbecc589b9866fd7cb7f0f24bd
Roman Pavlovskyi <roman@pumpup.co>
2019-01-29 12:28:34 -0800

    :recycle: [Root App] Move to /components

---
+++ src/components/root/index.style.js
@@ -0,0 +1,10 @@
+import STYLE from 'constants/style'
+
+const style = {
+  base: {
+    ...STYLE.ABSOLUTE_FILL,
+    backgroundColor: STYLE.COLOR.WHITE,
+  },
+}
+
+export default style
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App'
+import App from 'components/app'
+
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
