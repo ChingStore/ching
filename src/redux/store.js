77d7c422f1ece364628c17626d8d423aa16865da
Roman Pavlovskyi <roman@pumpup.co>
2019-01-30 00:52:02 -0800

    :bug: :recycle: [createStore] Fix redux logger, move firebase init out of config

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,35 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
 import * as Redux from 'redux'
-import ReduxLogger from 'redux-logger'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import * as ReduxLogger from 'redux-logger'
 import { default as ReduxThunk } from 'redux-thunk'
 import * as ReduxDevtoolsExtension from 'redux-devtools-extension'
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
-import { reduxFirestore, getFirestore } from 'redux-firestore'
 
 import rootReducer from 'redux/reducers'
 import firebaseConfig from 'config/firebase'
 
+firebase.initializeApp(firebaseConfig)
+firebase.firestore().enablePersistence()
+
+const reduxLogger = ReduxLogger.createLogger({
+  collapsed: true,
+  duration: true,
+  logErrors: true,
+  diff: true,
+})
+
 const store = Redux.createStore(
   rootReducer,
   ReduxDevtoolsExtension.composeWithDevTools(
+    reduxFirestore(firebase),
+    reactReduxFirebase(firebase),
     Redux.applyMiddleware(
       ReduxThunk.withExtraArgument({ getFirebase, getFirestore }),
-      ReduxLogger
-    ),
-    reduxFirestore(firebaseConfig),
-    reactReduxFirebase(firebaseConfig)
+      reduxLogger
+    )
   )
 )
 
