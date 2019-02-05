cc5e7d868651126bf2efa6fea49d599feb6323b9
pycckuu <is.markelov@gmail.com>
2019-02-05 01:44:02 +0100

    :construction: initial work on the add scene

--- src/components/add/index.js
+++ src/components/add/index.js
@@ -1,10 +1,74 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
 import React from 'react'
+import InputField from 'components/common/input-field'
+import style from './index.style.js'
 
 export default class Add extends React.PureComponent {
+  state = {}
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    })
+  }
+
+  renderTitle = () => {
+    return (
+      <div css={style.title}>
+        <p css={style.title__text}>Add an Item</p>
+      </div>
+    )
+  }
+
+  renderForm = () => {
+    return (
+      <div css={style.inputForm}>
+        <div css={style.inputForm_firstRow}>
+          <InputField
+            onChange={this.handleChange}
+            id="name"
+            placeholder="Item name"
+            labelText="Name"
+          />
+        </div>
+        <div css={style.inputForm_secondRow}>
+          <div css={style.inputForm_secondRow_firstColumn}>
+            <InputField
+              onChange={this.handleChange}
+              id="price"
+              placeholder="price"
+              labelText="Price USD"
+            />
+          </div>
+          <div css={style.inputForm_secondRow_secondColumn}>
+            <InputField
+              onChange={this.handleChange}
+              id="Quantity"
+              placeholder="Qty."
+              labelText="Quantity"
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  renderPhoto = () => {
+    return <div css={style.photo}>Photo</div>
+  }
+
+  renderFooter = () => {
+    return <div css={style.footer}>Footer</div>
+  }
+
   render() {
     return (
-      <div>
-        <div>I{"'"}m the add scene!</div>
+      <div css={style.base}>
+        {this.renderTitle()}
+        {this.renderForm()}
+        {this.renderPhoto()}
+        {this.renderFooter()}
       </div>
     )
   }
---
+++ src/components/add/index.style.js
@@ -0,0 +1,71 @@
+import STYLE from 'constants/style'
+
+export default {
+  base: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+  },
+
+  title: {
+    display: 'flex',
+    flex: 0.25,
+    width: '80%',
+  },
+
+  title__text: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    width: '80%',
+    fontWeight: 'bold',
+
+    fontSize: 32,
+    color: STYLE.COLOR.BLUE,
+  },
+
+  inputForm: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '80%',
+    flex: 0.25,
+  },
+
+  inputForm_firstRow: {
+    width: '100%',
+    flex: 0.5,
+  },
+
+  inputForm_secondRow: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flex: 0.5,
+  },
+
+  inputForm_secondRow_firstColumn: {
+    flex: 0.4,
+    border: '1px solid',
+  },
+
+  inputForm_secondRow_secondColumn: {
+    flex: 0.4,
+    width: '20%',
+    border: '1px solid',
+  },
+
+  photo: {
+    width: '80%',
+    flex: 0.25,
+    border: '1px solid',
+  },
+
+  footer: {
+    width: '80%',
+    flex: 0.25,
+    border: '1px solid',
+  },
+}
