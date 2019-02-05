02d2c45b8fe0f6dde4a6b01b520a32b33286d208
Aaron Anderson <aaronlynanderson@gmail.com>
2019-02-04 19:38:27 -0600

    :art: [onboarding-tweaks] constants for WalletButton

---
+++ src/constants/wallet_button.js
@@ -0,0 +1,36 @@
+// @flow
+
+import Icon from 'components/common/icon'
+import STYLE from './style.js'
+
+const TYPE = {
+  COINBASE: 'Coinbase',
+  STATUS: 'Status',
+  TRUST: 'Trust',
+}
+const TYPES = Object.values(TYPE)
+
+const LINK = {
+  [TYPE.COINBASE]: 'https://wallet.coinbase.com/',
+  [TYPE.STATUS]: 'https://get.status.im/',
+  [TYPE.TRUST]: 'https://trustwallet.com/',
+}
+
+const ICON = {
+  [TYPE.COINBASE]: Icon.Coinbase,
+  [TYPE.STATUS]: Icon.Status,
+  [TYPE.TRUST]: Icon.Trust,
+}
+
+const COLOR = {
+  [TYPE.COINBASE]: STYLE.COLOR.COINBASE,
+  [TYPE.STATUS]: STYLE.COLOR.STATUS,
+  [TYPE.TRUST]: STYLE.COLOR.TRUST,
+}
+
+export default {
+  TYPES,
+  LINK,
+  ICON,
+  COLOR,
+}
