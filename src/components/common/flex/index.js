51b5f9e894929b3e667dc472a23427849c33bec8
Roman Pavlovskyi <roman@pumpup.co>
2019-02-04 21:09:58 -0800

    :star: [Flex] Add spaceBetween and innerRef props

--- src/components/common/flex/index.js
+++ src/components/common/flex/index.js
@@ -1,21 +1,43 @@
+// @flow
+
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
-import React from 'react'
+import * as React from 'react'
 
-class Flex extends React.Component {
+type PropsType = {
+  auto?: boolean,
+  children: React.Node,
+  column?: boolean,
+  grow?: boolean,
+  innerRef?: React.Ref<'div'>,
+  spaceBetween?: boolean,
+  wrap?: boolean,
+}
+
+class Flex extends React.PureComponent<PropsType> {
   render = () => {
-    const { column, children, auto, wrap } = this.props
+    const {
+      auto,
+      children,
+      column,
+      grow,
+      innerRef,
+      spaceBetween,
+      wrap,
+    } = this.props
 
     const flexStyle = {
       display: 'flex',
       flex: auto && 'auto',
+      flexGrow: grow && 1,
       flexDirection: column && 'column',
       flexWrap: wrap && 'wrap',
+      justifyContent: spaceBetween && 'space-between',
     }
     return (
       // NOTE: The style from parent component takes precedence here
       // @see https://emotion.sh/docs/css-prop#style-precedence
-      <div css={flexStyle} {...this.props}>
+      <div css={flexStyle} ref={innerRef} {...this.props}>
         {children}
       </div>
     )
