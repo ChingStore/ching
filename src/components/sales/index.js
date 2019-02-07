1f91dcf4aa76cc5ca8a263d2edb2dc16a343c825
Roman Pavlovskyi <roman@pumpup.co>
2019-02-06 19:38:43 -0800

    :star: [Sales] Make it combine sales-report and orders

---
+++ src/components/sales/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import SalesReport from '../sales-report'
+import Orders from '../orders'
+
+export default () => (
+  <div>
+    <SalesReport />
+    <Orders />
+  </div>
+)
