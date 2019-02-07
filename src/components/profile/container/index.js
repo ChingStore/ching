576e447d942020e6c9d5366626e85da106d7af09
Aaron Anderson <aaronlynanderson@gmail.com>
2019-02-07 02:01:02 -0600

    :sparkles: [profile-scene] LogOut calls authActions.signOut()

--- src/components/profile/container/index.js
+++ src/components/profile/container/index.js
@@ -1,17 +1,23 @@
 import * as ReactRedux from 'react-redux'
+import PropTypes from 'prop-types'
 import selectors from 'redux/selectors'
 import authActions from 'redux/actions/auth'
-import SignUp from '..'
+import Profile from '..'
+
+Profile.propTypes = {
+  signOut: PropTypes.func,
+  authError: PropTypes.string,
+}
 
 const mapStateToProp = state => ({
   authError: selectors.getAuthError(state),
 })
 
 const mapDispatchToProps = dispatch => ({
-  signUp: credentials => dispatch(authActions.signUp(credentials)),
+  signOut: dispatch(authActions.signOut()),
 })
 
 export default ReactRedux.connect(
   mapStateToProp,
   mapDispatchToProps
-)(SignUp)
+)(Profile)
