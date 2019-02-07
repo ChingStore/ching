c471711bc439ca7d9844a58c269a4f2eaf400ebd
pycckuu <is.markelov@gmail.com>
2019-02-07 02:06:25 +0100

    :art: alphabetical order of icons

--- src/components/common/icon/index.js
+++ src/components/common/icon/index.js
@@ -3,25 +3,25 @@
 import Coinbase from './coinbase'
 import Cross from './cross'
 import DashedBox from './dashed-box'
+import Edit from './edit'
 import HomeArt from './home-art'
 import Login from './login'
+import NoPhoto from './no-photo'
+import Plus from './plus'
 import Profile from './profile'
 import Sales from './sales'
 import Status from './status'
+import Store from './store'
 import StoreArtBackground from './store-art-background'
 import StoreArtTag from './store-art-tag'
 import Trust from './trust'
-import Store from './store'
-import Plus from './plus'
-import Edit from './edit'
-import NoPhoto from './no-photo'
 import UploadCloud from './upload-cloud'
 
 export default {
   Coinbase,
-  Edit,
   Cross,
   DashedBox,
+  Edit,
   HomeArt,
   Login,
   NoPhoto,
