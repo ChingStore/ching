d48be457366a2c3fdfe01fad083bf3e1ba9574d6
pycckuu <is.markelov@gmail.com>
2019-02-12 01:16:13 +0100

    :art: checkmark

---
+++ src/components/common/checkmark/index.js
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+import React from 'react'
+
+class Checkmark extends React.Component {
+  render() {
+    return (
+      <div>
+        <svg
+          id="svg"
+          viewBox="0 0 100 100"
+          css={css`
+            height: 140px;
+            width: 140px;
+            left: 55%;
+            top: 40%;
+            position: absolute;
+            transform: translate(-50%, -50%);
+          `}
+        >
+          <path
+            id="checkmark"
+            className="check"
+            fill="none"
+            stroke="#009688"
+            strokeWidth="6"
+            d="M64.5,32.4L32.6,64.3L18.4,50"
+          />
+        </svg>
+      </div>
+    )
+  }
+}
+
+export default Checkmark
