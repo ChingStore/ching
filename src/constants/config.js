4225d5b10b92ea89997ee3cd34d6a64c3b3153c3
Roman Pavlovskyi <roman@pumpup.co>
2019-03-07 19:57:47 -0800

    :star: [Staging] Fix heroku env variables

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,15 +1,21 @@
 // @flow
 
-const { NODE_ENV, NODE_PUBLIC_URL, REACT_APP_API_URL } = process.env
+const {
+  NODE_ENV,
+  REACT_APP_ENV,
+  NODE_PUBLIC_URL,
+  REACT_APP_PUBLIC_URL,
+} = process.env
 
-const IS_PRODUCTION = NODE_ENV === 'production'
+const IS_PRODUCTION = REACT_APP_ENV === 'production'
 const PUBLIC_URL =
-  NODE_PUBLIC_URL || REACT_APP_API_URL || 'https://app.ching.store'
+  NODE_PUBLIC_URL || REACT_APP_PUBLIC_URL || 'https://app.ching.store'
 
 console.log({
   NODE_ENV,
+  REACT_APP_ENV,
   NODE_PUBLIC_URL,
-  REACT_APP_API_URL,
+  REACT_APP_PUBLIC_URL,
 
   IS_PRODUCTION,
   PUBLIC_URL,
