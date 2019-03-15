e916d91f4617dcfbc16e3b3094854415775e81aa
pycckuu <is.markelov@gmail.com>
2019-03-15 01:00:23 +0100

    :sparkles: oauth icons

---
+++ src/components/common/icon/facebook.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Facebook = () => (
+  <svg
+    width="14"
+    height="22"
+    viewBox="0 0 14 22"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M4.03153 22L4 12H0V8H4V5.5C4 1.7886 6.29832 0 9.60914 0C11.1951 0 12.5581 0.118075 12.9553 0.17085V4.04948L10.6591 4.05053C8.85845 4.05053 8.50981 4.90615 8.50981 6.16172V8H13.75L11.75 12H8.50981V22H4.03153Z"
+      fill="white"
+    />
+  </svg>
+)
+
+export default Facebook
