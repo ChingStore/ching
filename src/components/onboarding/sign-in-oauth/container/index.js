413606fe76ec1ca281bac51e72f35ce29f84ef45
pycckuu <is.markelov@gmail.com>
2019-03-17 21:20:13 +0100

    :art: signin scene

--- src/components/onboarding/sign-in-oauth/container/index.js
+++ src/components/onboarding/sign-in-oauth/container/index.js
@@ -1,23 +1,25 @@
 import * as ReactRedux from 'react-redux'
+import * as Reselect from 'reselect'
 import PropTypes from 'prop-types'
 import authActions from 'redux/actions/auth'
 import selectors from 'redux/selectors'
-import SignIn from '..'
+import SignInOauth from '..'
 
-SignIn.propTypes = {
-  signIn: PropTypes.func,
+SignInOauth.propTypes = {
+  signInWithOauth: PropTypes.func,
   authError: PropTypes.string,
 }
 
-const mapStateToProp = state => ({
-  authError: selectors.getAuthError(state),
+const mapStateToProps = Reselect.createStructuredSelector({
+  userId: selectors.users.currentId,
+  storeId: selectors.users.currentStoreId,
 })
 
 const mapDispatchToProps = dispatch => ({
-  signIn: credentials => dispatch(authActions.signIn(credentials)),
+  signInWithOauth: service => dispatch(authActions.signInWithOauth(service)),
 })
 
 export default ReactRedux.connect(
-  mapStateToProp,
+  mapStateToProps,
   mapDispatchToProps
-)(SignIn)
+)(SignInOauth)
--- src/components/onboarding/sign-in-oauth/index.js
+++ src/components/onboarding/sign-in-oauth/index.js
@@ -1,16 +1,31 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
 import React from 'react'
+import ReactReduxFirebase from 'react-redux-firebase'
 import FooterButton from 'components/common/footer-button'
 import ROUTE from 'constants/route'
+import PROVIDER from 'constants/auth-provider'
 import BackButton from 'components/common/back-button'
 import Icon from 'components/common/icon'
 import style from './index.style.js'
 
-export default class SignIn extends React.Component {
+export default class SignInOauth extends React.Component {
+  state = {
+    refreshIntervalId: null,
+  }
+
   handleSignInWithEmail = async () => {
     const { history } = this.props
     history.push(ROUTE.PATH.SIGN_IN)
+  }
+
+  handleSignInOauth = async service => {
+    const { signInWithOauth } = this.props
+    try {
+      signInWithOauth(service)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   renderTitle = () => {
@@ -27,6 +42,28 @@ export default class SignIn extends React.Component {
     return <div css={style.buttons}>{this.renderContinueButton()}</div>
   }
 
+  checkAuthStatusAndStage = () => {
+    const { userId, storeId, history } = this.props
+
+    if (ReactReduxFirebase.isLoaded()) {
+      clearInterval(this.refreshIntervalId)
+
+      if (userId && storeId) {
+        history.push(ROUTE.PATH.STORE)
+      }
+
+      if (userId && !storeId) {
+        history.push(ROUTE.PATH.SIGN_UP_STORE)
+      }
+    }
+  }
+
+  componentDidMount = () => {
+    this.refreshIntervalId = setInterval(() => {
+      this.checkAuthStatusAndStage()
+    }, 1000)
+  }
+
   renderContinueButton = () => {
     return (
       <div css={style.buttons}>
@@ -35,14 +72,24 @@ export default class SignIn extends React.Component {
         </FooterButton>
         <div>or</div>
 
-        <FooterButton onClick={this.signInWithGoogle} css={style.googleButton}>
+        <FooterButton
+          onClick={() => {
+            this.handleSignInOauth(PROVIDER.GOOGLE)
+          }}
+          css={style.googleButton}
+        >
           <div css={style.googleButton_icon}>
             <Icon.Google />
           </div>
           <div css={style.googleButton_text}>Log in with Google</div>
         </FooterButton>
 
-        <FooterButton onClick={this.signInWithGoogle} css={style.githubButton}>
+        <FooterButton
+          onClick={() => {
+            this.handleSignInOauth(PROVIDER.GITHUB)
+          }}
+          css={style.githubButton}
+        >
           <div css={style.githubButton_icon}>
             <Icon.Github />
           </div>
@@ -50,7 +97,9 @@ export default class SignIn extends React.Component {
         </FooterButton>
 
         <FooterButton
-          onClick={this.signInWithGoogle}
+          onClick={() => {
+            this.handleSignInOauth(PROVIDER.FACEBOOK)
+          }}
           css={style.facebookButton}
         >
           <div css={style.facebookButton_icon}>
