b22d74425f8cfe4d187000d1e7dc83b3dd2dc916
pycckuu <is.markelov@gmail.com>
2019-03-20 13:15:07 +0100

    :ok_hand: removed PropTypes

--- src/components/onboarding/sign-in-oauth/container/index.js
+++ src/components/onboarding/sign-in-oauth/container/index.js
@@ -1,14 +1,8 @@
 import * as ReactRedux from 'react-redux'
 import * as Reselect from 'reselect'
-import PropTypes from 'prop-types'
 import currentUser from 'redux/actions/current-user'
 import selectors from 'redux/selectors'
 import SignInOAuth from '..'
-
-SignInOAuth.propTypes = {
-  signInWithOAuth: PropTypes.func,
-  authError: PropTypes.string,
-}
 
 const mapStateToProps = Reselect.createStructuredSelector({
   userId: selectors.users.currentId,
